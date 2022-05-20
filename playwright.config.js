28c6505e31e232f571c730750dc7459019d34187
Martin Zlámal <mrtnzlml@gmail.com>
2022-05-19 19:57:50 -0500

    Abacus BO: enable minimal Playwright test
    Just to make sure the application can start successfully.
    
    adeira-source-id: 858887a5f76f556105873856a54d4f68781ad381

---
+++ playwright.config.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line ft-flow/require-valid-file-annotation,import/no-extraneous-dependencies
+const { devices } = require('@playwright/test');
+
+const config = {
+  testDir: 'tests',
+  testMatch: '**.play.js',
+  forbidOnly: !!process.env.CI,
+  reporter: process.env.CI ? 'github' : 'list',
+  retries: process.env.CI ? 2 : 0,
+  webServer: {
+    command: process.env.CI ? 'yarn run build && yarn run start' : 'yarn run dev',
+    port: 5001,
+    timeout: 120 * 1000, // milliseconds
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: devices['Desktop Chrome'],
+    },
+    {
+      name: 'Mobile Chrome',
+      use: devices['Pixel 5'],
+    },
+  ],
+};
+
+module.exports = config;
